fdef150f9bb65370f49f001bc1c8e454dbea91ef
Mingu Seo <tjalsrnek@gmail.com>
2021-07-22 01:24:53 +0900

    implement MemoDetailViewModel

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -28,6 +28,29 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
     func bindViewModel() {
         
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.contents
+            .bind(to: listTableView.rx.items) { tableView, row, value in
+                switch row {
+                //메모 컨텐츠 보여주기
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                    
+                //Date 보여주기
+                case 1:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
     }
     
 }
--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,8 +6,31 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import Action
 
-
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
     
+    //날짜를 문자로 변경할 때 사용
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }
+    
+    //메모를 편집후, 다시 메모 보기 화면으로 오기 위해선 새로운 문자열 배열을 방출해야 한다.
+    //일반 옵저버블로 선언하면 그렇게 되지 않기 때문에 subject로 선언
+    var contents: BehaviorSubject<[String]>
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value:[memo.content, formatter.string(from: memo.insertDate)])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
