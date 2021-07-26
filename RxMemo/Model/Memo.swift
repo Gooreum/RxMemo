66fbb9a562551330973fde6e1d60d100b14f6c08
Mingu Seo <tjalsrnek@gmail.com>
2021-07-26 17:34:36 +0900

    exchange storage to core data storage

--- RxMemo/Model/Memo.swift
+++ RxMemo/Model/Memo.swift
@@ -9,6 +9,8 @@ import Foundation
 
 //RxDataSources는 Tableview, collectionview에 바인딩할 수 있는 데이터 소스를 제공한다. -> 데이터 소스에 저장되는 모든 데이터는 반드시 IdentifiableType 프로토콜을 채용해야 한다.
 import RxDataSources
+import CoreData
+import RxCoreData
 
 struct Memo: Equatable, IdentifiableType {
         
@@ -26,5 +28,36 @@ struct Memo: Equatable, IdentifiableType {
     init(original: Memo, updateContent: String) {
         self = original
         self.content = updateContent
+    }
+}
+
+extension Memo: Persistable {
+    public static var entityName: String {
+        return "Memo"
+    }
+    
+    static var primaryAttributeName: String {
+        return "identity"
+    }
+    
+    //인스턴스 초기화 하는 생성자
+    init( entity: NSManagedObject) {
+        content = entity.value(forKey: "content") as! String
+        insertDate = entity.value(forKey: "insertDate") as! Date
+        identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    func update(_ entity: NSManagedObject) {
+        entity.setValue(content, forKey: "content")
+        entity.setValue(insertDate, forKey: "insertDate")
+        entity.setValue("\(insertDate.timeIntervalSinceReferenceDate)", forKey: "identity")
+        
+        do {
+            //CoreData를 RxCoreData로 구현할 시 주의해야 할 점 :
+            //RxCoreData는 context를 알아서 저장해주기 때문에 save를 직접 호출할 필요가 없음, 그런데 지금 구현하고 있는 이 코드는 RxCoreData를 사용하고 있지 않음. 그래서 Save 메서드를 직접 호출해야 한다. 그렇지 않으면 경우에 따라서 update한 내용이 사라질 수 있다.
+            try entity.managedObjectContext?.save()
+        } catch  {
+            print(error)
+        }
     }
 }
---
+++ RxMemo/Service/CoreDataStorage/CoreDataStorage.swift
@@ -0,0 +1,83 @@
+//
+//  CoreDataStorage.swift
+//  RxMemo
+//
+//  Created by Mingu Seo on 2021/07/26.
+//
+
+import Foundation
+import RxCoreData
+import RxSwift
+import CoreData
+
+class CoreDataStorage : MemoStorageType {
+    
+    let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+               
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    //Main Context 속성 추가
+    private var mainContext: NSManagedObjectContext {
+        //Container가 생성한 뷰 컨텍스트를 그대로 리턴한다.
+        return persistentContainer.viewContext
+    }
+    
+    // MARK: 기본 CRUD ---
+    
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo> {
+        let memo = Memo(content: content)
+        
+        do {
+            _ = try mainContext.rx.update(memo)
+            // 에러 : Instance method 'update' requires that 'Memo' conform to 'Persistable'
+            return Observable.just(memo)
+        } catch {
+            return Observable.error(error)
+        }
+    }
+    
+    @discardableResult
+    func memoList() -> Observable<[MemoSectionModel]> {
+        return mainContext.rx.entities(Memo.self, sortDescriptors: [NSSortDescriptor(key: "insertDate", ascending: false)])
+            .map { results in [MemoSectionModel(model: 0, items: results)] }
+    }
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo> {
+        let updated = Memo(original: memo, updateContent: content)
+        
+        do {
+            _ = try mainContext.rx.update(updated)
+            // 에러 : Instance method 'update' requires that 'Memo' conform to 'Persistable'
+            return Observable.just(updated)
+        } catch {
+            return Observable.error(error)
+        }
+    }
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo> {
+        
+        
+        do {
+            try mainContext.rx.delete(memo)
+            return Observable.just(memo)
+        } catch {
+            return Observable.error(error)
+        }
+    }
+}
